6170b774beaeab368b37d821d5d2857ea8ddcce2
Marat Dolotov <marat@it-link45.ru>
2025-03-17 14:02:24 +0500

    avante color config

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -16,6 +16,8 @@ return {
     highlights = {
       init = { -- this table overrides highlights in all themes
         -- Normal = { bg = "#000000" },
+        AvanteConflictIncoming = { bg = "#FF0000" }, -- Красный фон
+        AvanteConflictCurrent = { bg = "#00FF00" },  -- Зеленый фон
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
